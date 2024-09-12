5299fa897d9ed86f32fa58fae3cb2a4534adda5b
Ghost_Math <141246042+TuzinFlxo@users.noreply.github.com>
2024-09-12 11:42:58 -0300

    Add files via upload

---
+++ src/app/_layout.js
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import { AppProvider } from "../hooks";
+
+export default function Layout() {
+    return (
+    <AppProvider>
+        
+        <Stack />
+        
+    </AppProvider>
+    );
+}
